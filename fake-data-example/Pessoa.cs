ef81da9c7106b8d02c8670ad9d38ff3b1a6ff010
Marcos Vinicius Belorio <marcos.belorio@ambevtech.com.br>
2021-05-11 22:46:08 -0300

    Add project files.

---
+++ fake-data-example/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fake_data_example
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ fake-data-example/Program.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace fake_data_example
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Faker<Pessoa>("pt_BR")
+                .RuleFor(x => x.Id, f => f.Random.Int(1))
+                .RuleFor(x => x.Nome, f => f.Name.FullName(Bogus.DataSets.Name.Gender.Male))
+                .RuleFor(x => x.Email, (f, u) => f.Internet.Email(u.Nome))
+                .RuleFor(x => x.DataNascimento, f => f.Date.Past(50))
+                .RuleFor(x => x.Telefone, f => f.Phone.PhoneNumberFormat())
+                .Generate();
+
+            Console.WriteLine(JsonConvert.SerializeObject(pessoa, Formatting.Indented));
+
+            List<Pessoa> pessoaLista = new Faker<Pessoa>("pt_BR")
+                .RuleFor(x => x.Id, f => f.Random.Int(1,100))
+                .RuleFor(x => x.Nome, f => f.Name.FullName())
+                .RuleFor(x => x.Email, (f, u) => f.Internet.Email(u.Nome))
+                .RuleFor(x => x.DataNascimento, f => f.Date.Between(Convert.ToDateTime("01/01/1990"), Convert.ToDateTime("01/01/2000")))
+                .RuleFor(x => x.Telefone, f => f.Phone.PhoneNumberFormat())
+                .Generate(10);
+
+            Console.WriteLine(JsonConvert.SerializeObject(pessoaLista, Formatting.Indented));
+        }
+    }
+}
